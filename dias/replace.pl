fa60414a95d144dc5d5937589be6692a02e6b283
hertzhaft <hertzhaft@localhost>
2019-12-03 22:56:35 +0100

    save status 3.12.

--- dias/replace.pl
+++ dias/replace.pl
@@ -10,17 +10,6 @@ use DDP;
 use Digest::SHA qw(sha256_hex);
 use JSON;
 
-my $h = {};
-my $text = b(path('./all_dias.tab')->slurp)->split(qr{\R});
-my $res = $text->each( sub {
-  my ($dia, $nr) = (split /\t/, $_);
-  say $dia if $h->{$dia};
-  $h->{$dia}++
-  });
-
-#my $text = b(path('./Numbers_all_by_dia.tab')->slurp)->split(qr{\R});
-#my $res = $text->sort->join("\n");
-#path('./all_dias.tab')->spurt($res);
 
 #my $text = b(path('./Numbers1.json')->slurp)->split(qr{\R});
 #
@@ -41,3 +30,42 @@ my $res = $text->each( sub {
 #$text =~ s/_r//g;
 #say length $text;
 #path('./dia_katalog.pm')->spurt($text);
+
+# Create all_dias
+#my $text = b(path('./Numbers_all_by_dia.tab')->slurp)->split(qr{\R});
+#my $res = $text->sort->join("\n");
+#path('./all_dias.tab')->spurt($res);
+
+# CHeck for double dias
+#my $IMG = '<img src="http://www.kunstgeschichte.hu-berlin.de/clickjob/SCHG_';
+#my $END = '_r.jpg">';
+#say '<html>';
+#my $h = {};
+#my $text = b(path('./all_dias.tab')->slurp)->split(qr{\R});
+#my $res = $text->each( sub {
+#  my ($dia, $nr) = (split /\s+/, $_);
+#  my $prev = $h->{$dia};
+#  say "<div>$dia$IMG$prev$END$prev$IMG$nr$END$nr</div>" if $prev && $prev ne $nr;
+#  $h->{$dia} = $nr;
+#  });
+#say '</html>';
+
+# CHeck for double box numbers
+#my $h = {};
+#my $text = b(path('./all_dias.tab')->slurp)->split(qr{\R});
+#my $res = $text->each( sub {
+#  my ($dia, $nr) = (split /\s+/, $_);
+#  my $prev = $h->{$nr};
+#  say $nr if $prev && $prev ne $dia;
+#  $h->{$nr} = $dia;
+#  });
+
+# Eliminate duplicates
+my $h = {};
+my $text = b(path('./all_dias.tab')->slurp)->split(qr{\R});
+my $res = $text->sort->each( sub {
+  my ($dia, $nr) = (split /\s+/, $_);
+  my $prev = $h->{$dia};
+  say "$dia\t$nr" if $dia =~ /^\D/ || !$prev;
+  $h->{$dia} = $nr;
+  });
